ad7f2638a459b714523a17f7c865a908bc0b0864
kaiyang154 <93995004+kaiyang154@users.noreply.github.com>
2021-12-28 22:11:10 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ComponentMappingPractice
+# MappingComponentPractice
 Created with CodeSandbox
---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      {emojipedia.map(createEntry)}
+      <dl className="dictionary"></dl>
+    </div>
+  );
+}
+
+export default App;
